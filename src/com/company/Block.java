79501e9e2b7e99facd15ec8259bd099c8b86ae38
Thai Phan <thai@outlook.com>
2018-01-01 14:14:20 +1100

    Add Block class attributes

--- src/com/company/Block.java
+++ src/com/company/Block.java
@@ -1,4 +1,61 @@
 package com.company;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Block {
+    private String prevBlock;
+    private String merkleRoot;
+    private LocalDateTime timestamp;
+    private Long bits;
+    private Long nonce;
+    private List<Transaction> transactions;
+
+    public String getPrevBlock() {
+        return prevBlock;
+    }
+
+    public void setPrevBlock(String prevBlock) {
+        this.prevBlock = prevBlock;
+    }
+
+    public String getMerkleRoot() {
+        return merkleRoot;
+    }
+
+    public void setMerkleRoot(String merkleRoot) {
+        this.merkleRoot = merkleRoot;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Long getBits() {
+        return bits;
+    }
+
+    public void setBits(Long bits) {
+        this.bits = bits;
+    }
+
+    public Long getNonce() {
+        return nonce;
+    }
+
+    public void setNonce(Long nonce) {
+        this.nonce = nonce;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
 }
