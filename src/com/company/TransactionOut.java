93f1405eb07f7ca7f61ab5dd9bf8323045eb37bc
Thai Phan <thai@outlook.com>
2018-01-01 17:14:04 +1100

    Flesh out transaction class

--- src/com/company/TransactionOut.java
+++ src/com/company/TransactionOut.java
@@ -1,4 +1,22 @@
 package com.company;
 
 public class TransactionOut {
+    private Long value;
+    private String script;
+
+    public Long getValue() {
+        return value;
+    }
+
+    public void setValue(Long value) {
+        this.value = value;
+    }
+
+    public String getScript() {
+        return script;
+    }
+
+    public void setScript(String script) {
+        this.script = script;
+    }
 }
